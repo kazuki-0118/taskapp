40266d9e87cc48713b55e95c34da69c3e653fdc9
kazuki.kawamura <k09046607339@yahoo.co.jp>
2021-01-31 21:11:36 +0900

    //通知を修正

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -93,31 +93,31 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
           return .delete
       }
     // Delete ボタンが押された時に呼ばれるメソッド
-       func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        // --- ここから ---
-           if editingStyle == .delete {
-               // 削除するタスクを取得する
-               let task = self.taskArray[indexPath.row]
+      func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+          // --- ここから ---
+          if editingStyle == .delete {
+              // 削除するタスクを取得する
+              let task = self.taskArray[indexPath.row]
 
-               // ローカル通知をキャンセルする
-               let center = UNUserNotificationCenter.current()
-               center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+              // ローカル通知をキャンセルする
+              let center = UNUserNotificationCenter.current()
+              center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
 
-               // データベースから削除する
-               try! realm.write {
-                   self.realm.delete(task)
-                   tableView.deleteRows(at: [indexPath], with: .fade)
-               }
+              // データベースから削除する
+              try! realm.write {
+                  self.realm.delete(task)
+                  tableView.deleteRows(at: [indexPath], with: .fade)
+              }
 
-               // 未通知のローカル通知一覧をログ出力
-               center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
-                   for request in requests {
-                       print("/---------------")
-                       print(request)
-                       print("---------------/")
-                   }
-               }
-           } // --- ここまで変更 ---
+              // 未通知のローカル通知一覧をログ出力
+              center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+                  for request in requests {
+                      print("/---------------")
+                      print(request)
+                      print("---------------/")
+                  }
+              }
+          } // --- ここまで変更 ---
        }
     // segue で画面遷移する時に呼ばれる
        override func prepare(for segue: UIStoryboardSegue, sender: Any?){
